93eb22427b58d36394f578652509a57ecf9456b0
Maciexyn <57116855+Maciexyn@users.noreply.github.com>
2022-12-19 23:20:25 +0100

    Reorganizacja plików

---
+++ O1 -/E2 -/PRZ -/PRZ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int v[1000001];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    for (int i = 1;i<=n;i++)
+    {
+        
+    }
+}
